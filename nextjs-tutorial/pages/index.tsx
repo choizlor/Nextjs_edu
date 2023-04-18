d53fe88596aa9293f2b5d5f595d8407bfc8a589b
choizlor <choizlor@gmail.com>
2023-04-19 01:04:48 +0900

    코딩앙마 nextjs #2 6분

--- nextjs-tutorial/pages/index.tsx
+++ nextjs-tutorial/pages/index.tsx
@@ -1,11 +1,37 @@
 import Head from "next/head";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ItemList from "@/src/component/ItemList";
+import { Divider, Header } from "semantic-ui-react";
 
 export default function Home() {
+  const [list, setList] = useState([]);
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+  function getData() {
+    axios.get(API_URL).then((res) => {
+      console.log(res);
+      setList(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <Head>
-        <title>HOME | 코딩앙마!</title>
+        <title>HOME | NEXT지우</title>
       </Head>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list} />
     </div>
   );
 }
+
+// axios
