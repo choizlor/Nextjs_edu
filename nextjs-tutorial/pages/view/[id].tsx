620ca481ba17e2c34ace0d2f368f9766b9f113fe
choizlor <choizlor@gmail.com>
2023-04-20 23:59:22 +0900

    코딩앙마 nextjs #2

--- nextjs-tutorial/pages/view/[id].tsx
+++ nextjs-tutorial/pages/view/[id].tsx
@@ -1,3 +1,11 @@
-export default function About() {
-  return <div>ID</div>;
-}
+import { useRouter } from "next/router";
+import Link from 'next/link'
+
+const Post = () => {
+  const router = useRouter();
+  const { pid } = router.query;
+
+  return <p>Post: {pid}</p>;
+};
+
+export default Post;
