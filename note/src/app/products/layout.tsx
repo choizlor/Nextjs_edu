d0973eb7fb17da45366e3d05eb254819cb1fb56e
choizlor <choizlor@gmail.com>
2023-04-25 15:32:56 +0900

    Metadata

--- note/src/app/products/layout.tsx
+++ note/src/app/products/layout.tsx
@@ -1,5 +1,14 @@
+import { Metadata } from "next";
 import Link from "next/link";
 import styles from "./layout.module.css";
+
+export const metadata: Metadata = {
+  title: "멋진 제품 사이트 | 전체 제품 확인",
+  description: "멋진 제품을 확인해보세요.",
+  icons: {
+    icon: '../public/favicon.ico'
+  }
+};
 
 export default function ProductsLayout({
   children,
