2f8b107a51f73506a3bb86c29089be4e993d23ee
choizlor <choizlor@gmail.com>
2023-04-25 12:04:34 +0900

    Dynamic routing

---
+++ note/src/app/products/[slug]/page.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function Pants({ params }: Props) {
+  return <h1>{params.slug} 페이지</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map(product => ({
+    slug: product,
+  }))
+}
--- note/src/app/products/pants/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Pants() {
-    return <h1>바지 페이지</h1>;
-  }
