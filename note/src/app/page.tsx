c99c3ce9803f8a2619229ae9a8842bca08863028
choizlor <choizlor@gmail.com>
2023-04-25 14:33:22 +0900

    Layout, Link

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import { notFound } from "next/navigation";
 import styles from "./page.module.css";
 
 export default function Home() {
-  notFound();
   return <h1>홈페이지!</h1>;
 }
