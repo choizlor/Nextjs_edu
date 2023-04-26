b8d4b430d2ea27a7df40b4ce93f1a71da5e01183
choizlor <choizlor@gmail.com>
2023-04-26 11:45:44 +0900

    12ver. rendering method

---
+++ note/src/pages/ssr.tsx
@@ -0,0 +1,32 @@
+import MeowArticle from "@/components/MeowArticle";
+import { getProducts, Product } from "@/service/products";
+import Link from "next/link";
+
+type Props = {
+  products: Product[];
+};
+
+// Client
+export default function SSRPage({ products }: Props) {
+  return (
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map(({ id, name }, index) => (
+          <li key={index}>
+            <Link href={`products/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <MeowArticle />
+    </>
+  );
+}
+
+// ServerSide
+export async function getServerSideProps() {
+  const products = await getProducts();
+  return {
+    props: { products },
+  };
+}
