3f7575f3b45c52319fc2f7a7c61af86790751132
choizlor <choizlor@gmail.com>
2023-04-27 00:36:03 +0900

    blog page 만들기

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image";
+import CarouselPosts from "@/components/CarouselPosts";
+import FeaturedPosts from "@/components/FeaturedPosts";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <section className="bg-gray-500">Main</section>;
+  return (
+    <>
+      <Hero />
+      {/* @ts-expect-error Server Component */}
+      <FeaturedPosts />
+      {/* @ts-expect-error Server Component */}
+      <CarouselPosts />
+    </>
+  );
 }
