4257e08a91e3206e15b64acd0a594e30d2e75406
choizlor <choizlor@gmail.com>
2023-04-26 10:18:50 +0900

    SSG rendering

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getProduct, getProducts } from "@/service/products";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -12,16 +13,22 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default function Pants({ params }: Props) {
-  if (params.slug === "nothing") {
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
+
+  if (!product) {
     notFound();
   }
-  return <h1>{params.slug} 페이지</h1>;
+
+  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+  return <h1>{product.name} 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
-  const products = ["pants", "skirt"];
+// SSG 비동기 가능
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
